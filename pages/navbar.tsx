530182e314bb70d74b84c934f316cb48037e0f5b
user <user@users-MacBook-Pro.local>
2023-04-13 18:19:58 +0300

    adding the navbar

--- pages/navbar.tsx
+++ pages/navbar.tsx
@@ -1,26 +1,40 @@
 import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar: React.FC = () => {
   return (
-    <div className="flex w-full h-16 bg-[#19092A] justify-between">
-      <div className="flex justify-between items-center gap-4 mx-8">
-        <Image alt="logo" src="/favicon.ico" width={45} height={45}></Image>
-        <h1 className="text-white">Store</h1>
-        <h1 className="text-white">Geners</h1>
-        <h1 className="text-white">Deals</h1>
-        <h1 className="text-white">About</h1>
+    <div className="flex flex-col md:flex-row md:justify-between w-full h-16 bg-[#000000]">
+      <div className="flex flex-col md:flex-row justify-between items-center mx-8 w-full md:w-auto">
+        <Image
+          style={{ position: "relative", top: "2px" }}
+          alt="logo"
+          src="/YsamplesLogo.png"
+          width={100}
+          height={100}
+        />
+        <h1 className="text-white cursor-pointer mb-2 md:mb-0 md:ml-4">
+          Store
+        </h1>
+        <h1 className="text-white cursor-pointer mb-2 md:mb-0 md:ml-4">
+          Genres
+        </h1>
+        <h1 className="text-white cursor-pointer mb-2 md:mb-0 md:ml-4">
+          Deals
+        </h1>
+        <h1 className="text-white cursor-pointer mb-2 md:mb-0 md:ml-4">
+          About
+        </h1>
       </div>
-      <div className="flex justify-center items-center gap-4 mx-8">
+      <div className="flex justify-center items-center gap-4 mx-8 md:w-auto">
         <h1 className="text-white flex gap-1">
-          <Image
-            alt="account"
-            src="/userIcon.svg"
-            width={20}
-            height={20}
-          ></Image>
+          <Image alt="account" src="/userIcon.svg" width={20} height={20} />
           Account
         </h1>
-        <h1 className="text-white">Cart</h1>
+        <h1 className="text-white cursor-pointer">
+          <FontAwesomeIcon icon={faShoppingCart} className="mx-2" />
+          Cart
+        </h1>
       </div>
     </div>
   );
